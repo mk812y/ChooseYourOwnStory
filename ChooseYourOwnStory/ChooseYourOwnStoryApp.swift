4aa3c673c746b6b0f1aaaad869117966b4579b9e
mk812y <mk812y@gmail.com>
2023-01-21 14:20:21 +0300

    Initial Commit

---
+++ ChooseYourOwnStory/ChooseYourOwnStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChooseYourOwnStoryApp.swift
+//  ChooseYourOwnStory
+//
+//  Created by Михаил Куприянов on 21.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChooseYourOwnStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
